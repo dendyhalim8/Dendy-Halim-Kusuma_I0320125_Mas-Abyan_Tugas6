c71bcae051f4a1c79a9d5fe4d86ae3cb995656f6
dendyhalim8 <79240184+dendyhalim8@users.noreply.github.com>
2021-04-10 07:47:35 +0700

    Add files via upload

---
+++ soal 2.py
@@ -0,0 +1,7 @@
+print("menghitung rata-rata, masukan nilai")
+x = int(input("> "))
+y = int(input("> "))
+for rata in (x,y):
+    rata = float((x+y)/2)
+print("Nilai rata-rata, dari %d dan %d"%(x,y))
+print("> ",rata)
